2bf9648ca24ffe56a85b00ae380ccfa6ffb2e8cc
Fabian Canas <fcanas@gmail.com>
2015-01-04 15:50:13 -0500

    Indicate a desire to drink

--- DrinkUp/GameScene.swift
+++ DrinkUp/GameScene.swift
@@ -7,39 +7,88 @@
 //
 
 import SpriteKit
+import CoreMotion
 
 class GameScene: SKScene {
+    
+    lazy var motionManager: CMMotionManager? = CMMotionManager()
+    
+    var touch: UITouch?
+    var label: SKLabelNode?
+    
+    func makeBlob() -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: "spot")
+        return node
+    }
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        label = SKLabelNode(fontNamed:"Futura")
+        label?.text = "Coffee?";
+        label?.fontSize = 65;
+        label?.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        self.scaleMode = SKSceneScaleMode.AspectFit
         
-        self.addChild(myLabel)
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody?.collisionBitMask = 0xffff
+        
+        motionManager?.deviceMotionUpdateInterval = 1.0/40.0
+        motionManager?.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { (motion: CMDeviceMotion!, error: NSError!) -> Void in
+            self.physicsWorld.gravity = CGVectorMake(CGFloat(motion.gravity.x * 9.8), CGFloat(motion.gravity.y * 9.8))
+            self.label?.zRotation = CGFloat(atan2(motion.gravity.y, motion.gravity.x) + Double(3.1415926 / 2.0))
+        })
+        
+        self.addChild(label!)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
+        for touch in touches {
+            let nodes = self.nodesAtPoint(touch.locationInNode(self))
+            for node in nodes {
+                if node as SKNode == label! {
+                    
+                    
+                    label!.text = (label!.text=="Coffee?") ? "Beer?":"Coffee?"
+                }
+            }
+        }
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        touch = touches.anyObject() as? UITouch
+        
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        if touch != nil && touches.containsObject(touch!) {
+            touch = nil
         }
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        if touch == nil || self.children.count > 300 {
+            return
+        }
+        
+        let location = touch!.locationInNode(self)
+        let sprite = SKSpriteNode(imageNamed:"spot")
+        sprite.xScale = 4
+        sprite.yScale = 4
+        sprite.blendMode = SKBlendMode.Add
+        
+        sprite.position = location
+        
+        let physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width/10)
+        
+        physicsBody.collisionBitMask = 1
+        physicsBody.restitution = 0.0
+        physicsBody.friction = 0.0
+        physicsBody.linearDamping = 0.005
+        physicsBody.density = 0.05
+        physicsBody.allowsRotation = false
+        
+        sprite.physicsBody = physicsBody
+        self.addChild(sprite)
     }
 }
--- DrinkUp/GameViewController.swift
+++ DrinkUp/GameViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SpriteKit
+import iAd
 
 extension SKNode {
     class func unarchiveFromFile(file : NSString) -> SKNode? {
@@ -26,30 +27,27 @@ extension SKNode {
 }
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
             let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+//            skView.showsFPS = true
+//            skView.showsNodeCount = true
             
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
             
             skView.presentScene(scene)
         }
+//        self.canDisplayBannerAds = true;
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
             return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
@@ -57,12 +55,12 @@ class GameViewController: UIViewController {
             return Int(UIInterfaceOrientationMask.All.rawValue)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
