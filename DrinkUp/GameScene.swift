579b3072d4c5a341d655cedb938352fb6d10164a
Fabian Canas <fcanas@gmail.com>
2015-01-06 21:34:51 -0500

    Let droplets pour out of the top of the screen
    Fix provisioning

--- DrinkUp/GameScene.swift
+++ DrinkUp/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     lazy var motionManager: CMMotionManager? = CMMotionManager()
     
@@ -21,16 +21,37 @@ class GameScene: SKScene {
         return node
     }
     
+    var killNode: SKNode?
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         label = SKLabelNode(fontNamed:"Futura")
         label?.text = "Coffee?";
         label?.fontSize = 65;
         label?.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        self.scaleMode = SKSceneScaleMode.AspectFit
+        scaleMode = SKSceneScaleMode.AspectFit
         
-        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-        self.physicsBody?.collisionBitMask = 0xffff
+        var collisionLoop = self.frame
+        collisionLoop.size.height += 40
+        collisionLoop.origin.y += 20
+        
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: collisionLoop)
+        physicsBody?.collisionBitMask = 0x2
+        
+        var originPoint = collisionLoop.origin
+        originPoint.y = collisionLoop.maxY
+        var oppositePoint = originPoint
+        oppositePoint.x += collisionLoop.width
+        
+        let top = SKPhysicsBody(edgeFromPoint: originPoint, toPoint: oppositePoint)
+        top.contactTestBitMask = 0x1
+        top.categoryBitMask = 0x1
+        killNode = SKNode()
+        killNode?.physicsBody = top
+        
+        physicsWorld.contactDelegate = self
+        
+        addChild(killNode!)
         
         motionManager?.deviceMotionUpdateInterval = 1.0/40.0
         motionManager?.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { (motion: CMDeviceMotion!, error: NSError!) -> Void in
@@ -38,7 +59,7 @@ class GameScene: SKScene {
             self.label?.zRotation = CGFloat(atan2(motion.gravity.y, motion.gravity.x) + Double(3.1415926 / 2.0))
         })
         
-        self.addChild(label!)
+        addChild(label!)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -47,15 +68,11 @@ class GameScene: SKScene {
             let nodes = self.nodesAtPoint(touch.locationInNode(self))
             for node in nodes {
                 if node as SKNode == label! {
-                    
-                    
                     label!.text = (label!.text=="Coffee?") ? "Beer?":"Coffee?"
                 }
             }
         }
-        
         touch = touches.anyObject() as? UITouch
-        
     }
     
     override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
@@ -65,8 +82,6 @@ class GameScene: SKScene {
     }
     
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-        
         if touch == nil || self.children.count > 300 {
             return
         }
@@ -81,14 +96,31 @@ class GameScene: SKScene {
         
         let physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width/10)
         
-        physicsBody.collisionBitMask = 1
+        physicsBody.collisionBitMask = 0x2
         physicsBody.restitution = 0.0
         physicsBody.friction = 0.0
         physicsBody.linearDamping = 0.005
         physicsBody.density = 0.05
         physicsBody.allowsRotation = false
+        physicsBody.contactTestBitMask = 0x1
+        physicsBody.categoryBitMask = 0x7
         
         sprite.physicsBody = physicsBody
         self.addChild(sprite)
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let spriteA = contact.bodyA.node as? SKSpriteNode
+        let spriteB = contact.bodyB.node as? SKSpriteNode
+        
+        // We need one and only one node to be a sprite
+        if (spriteA != nil && spriteB != nil) {
+            return
+        }
+        
+        if contact.bodyA.node == killNode || contact.bodyB.node == killNode {
+            spriteA?.removeFromParent()
+            spriteB?.removeFromParent()
+        }
     }
 }
